a6be95bb516ce7631368a2e7cd9f745cc056525d
Khalil <khalilfoulks@gmail.com>
2023-08-15 16:48:56 -0400

    add event handlers

--- scoreboard_app/src/components/PlayerForm.js
+++ scoreboard_app/src/components/PlayerForm.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlayerForm(){
+    const [value, setValue] = useState("");
+
+    const inputChange = (e) => {
+        console.log(e)
+        setValue(e.target.value)
+    } 
+
+    const submitForm = (e) => {
+        e.preventDefault();
+    }
+
     return(
-        <form>
+        <form onSubmit={submitForm}>
             <input
                 type="text"
+                value = {value}
                 placeholder="Enter a player's name"
+                onChange = {inputChange}
             />
             <input
                 type="submit"
