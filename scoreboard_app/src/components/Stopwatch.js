076623b9abd1e63162beb1d3bb0cfd425c3c9d23
Khalil <khalilfoulks@gmail.com>
2023-08-28 15:56:48 -0400

    add start and stop functionality

--- scoreboard_app/src/components/Stopwatch.js
+++ scoreboard_app/src/components/Stopwatch.js
@@ -1,11 +1,16 @@
 import React, {useState} from "react";
 
 function Stopwatch(){
+    const [isRunning, setIsRunning] = useState(false);
     return (
         <div className="stopwatch">
             <h2>Stopwatch</h2>
             <span className="stopwatch-time">0</span>
-            <button>Start</button>
+            <button onClick={() => setIsRunning(!isRunning)}>
+                {
+                    isRunning ? "Stop" : "Start"
+                }
+            </button>
             <button>Reset</button>
         </div>
     )
